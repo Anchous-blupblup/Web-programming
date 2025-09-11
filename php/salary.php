cde5a4ef0d3c2d21568e06cc56e864329f1e2b7a
Anchous <anja.zibrova@gmail.com>
2025-09-11 13:10:14 +0300

    PHP task 3

--- php/salary.php
+++ php/salary.php
@@ -1,8 +1,22 @@
-<?php
-$hourly wage = $_POST["hourly wage"] ?? 0;
-$hourly rate = $_POST["hourly rate"] ?? 0;
-$total wage = $hourly wage * $hourly rate;
-echo "Total wage: " . $total wage;
-echo "Total salary without weekend bonuses:" . $total wage;
-echo "Total salary with weekend bonuses:" . $total wage;
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Salary</title>
+  </head>
+  <body>
+    <?php
+$hourly_wage = $_POST["hourly_wage"] ?? 0;
+$hourly_rate = $_POST["hourly_rate"] ?? 0;
+$weekend_allowance = $_POST["weekend_allowance"] ?? 0;
+$weekends = $_POST["weekends"] ?? 0;
+
+$total_wage = $hourly_wage * $hourly_rate;
+$total_without_bonus = $hourly_wage * $hourly_rate;
+$total_with_bonus = $total_without_bonus + ($weekend_allowance * $weekends);
+
+echo "Total wage: " . $total_wage . "<br>"; 
+echo "Total salary without weekend bonuses:" . $total_without_bonus . "<br>"; 
+echo "Total salary with weekend bonuses:" . $total_with_bonus . "<br>"; 
 ?>
+</body>
+</html>
--- php/salary_calculator.php
+++ php/salary_calculator.php
@@ -5,10 +5,10 @@
   </head>
   <body>
     <form action="salary.php" method="post">
-      Hourly salary: <input type="text" name="hourly rate"> <br>
-      Number of hours: <input type="text" name="hourly wage"> <br>
-      Weekend allowance: <input type="text" name="hourly salary"> <br>
-      Number of weekends: <input type="text" name="hourly salary"> <br>
+      Hourly salary: <input type="text" name="hourly_rate"> <br>
+      Number of hours: <input type="text" name="hourly_wage"> <br>
+      Weekend allowance: <input type="text" name="weekend_allowance"> <br>
+      Number of weekends: <input type="text" name="weekends"> <br>
       <input type="submit" value="Submit">
     </form>
   </body>
